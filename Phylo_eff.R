7a4938f250d79f23ab8893481faee91338bd86a8
oliviakimber <olivia_kimber@live.com.au>
2017-01-19 11:53:11 +1000

    Add files via upload

--- Phylo_eff.R
+++ Phylo_eff.R
@@ -1,6 +1,6 @@
 # download the packages
 library(ape)
-#library(geiger)
+library(geiger)
 #library(nlme)
 library(phytools)
 #library(picante)
@@ -23,7 +23,7 @@ pruned<-drop.tip(myo1,matches$tree_not_data)
 
 
 # Plot the pruned tree
-plot(pruned)
+plot(pruned, cex = 0.5)
 
 # order the data in mydata to be in the same sequence as in the tree
 trait_order<-data.frame(mydata[pruned$tip.label,])
@@ -32,21 +32,16 @@ head(trait_order) # check that this worked
 head(pruned$tip.label) # by comparing it to this
 
 # make a colour palette
-palette(c("#2c7fb8", "#feb24c", "#238b45", "#de2d26", "grey", "#88419d", "#41b6c4", "#df65b0", "#225ea8", "#fe9929", "#ef6548", "#78c679", "#ec7014", "black")) 
+palette(c("#2c7fb8", "#238b45", "#feb24c", "#de2d26", "#88419d")) 
 
 #### Question 1: IS there a relationship betweenn mass and eff?? ####
 # Scatterplot- same as the one we looked at previously, but coloured by diet
 summary(fit<-lm(eff~log10(mass),mydata)) # create a linear model, save the intercept and slope to 'fit'
-plot(eff~log10(mass),mydata, pch=19, col=mydata$diet, font.lab = 2, las = 1, ylab = expression(bold(paste("Effective hind-limb length"))), xlab = expression(bold(paste("Log10(body mass)"))))
-mydata$diet = factor(mydata$diet, levels = c('1','2', '3'),labels = c("Carnivore", "Herbivore", "Omnivore"))
-legend("topleft", legend=levels(mydata$diet), pch = 19, bty = "n", col = c(1:3))
+plot(eff~log10(mass),mydata, pch=19, col=mydata$diet, font.lab = 2, las = 1, ylab = expression(bold(paste("Effective hindlimb length"))), xlab = expression(bold(paste("Log10(body mass)"))))
+mydata$diet1 = factor(mydata$diet, levels = c('1','2', '3'),labels = c("Carnivore", "Herbivore", "Omnivore"))
+legend("topleft", legend=levels(mydata$diet1), pch = 19, bty = "n", col = c(1:3))
 abline(fit, col="red", lwd=2)
-# or colour by order
-plot(eff~log10(mass),mydata, pch=19, col=mydata$order, font.lab = 2, las = 1, ylab = expression(bold(paste("Effective hind-limb length"))), xlab = expression(bold(paste("Log10(body mass)"))))
-legend(2.9,0.75,legend=levels(mydata$order), cex = 0.65, pch = 19, col = c(1:14), bty = "n")
-abline(fit, col="red", lwd=2) # add a red abline to the data with 'fit's information
-# There's an upward trend- a slope of 0.08, and the summary(lm) indicates that this is significant (p < 2.2e-16)
-
+LMresid <- fit$residuals
 
 #### Question 2: ####
 # is there an effect of phylogeny on mass? 
@@ -84,8 +79,15 @@ head(diet) # make sure it worked by checking the original diet dataframe
 head(dietV) # against the massV dataframe
 # phylosig randomises the massV data throughout the phylogenetic tree 10000 times to work out how likely
 # it is that the diet is in this order
-phylosig(pruned, dietV, method="lambda", test=TRUE, nsim=10000)
+
+PHY <- phylosig(pruned, dietV, method="lambda", test=TRUE, nsim=10000, start=c(1,1))
 # very unlikely that effective limb length is in this order by chance- the p-value is 6.614822e-43
+x<-anc.Bayes(pruned, massV, ngen = 10000)
+estimates<-colMeans(x[21:nrow(x),])
+
+x<-anc.Bayes(pruned, effV, ngen = 10000)
+estimates<-colMeans(x[21:nrow(x),])
+
 
 # Performs phylogenetic RMA regression.
 # Phyl.RMA accounts for the effect of phylogeny on effective hip height as a function of mass. 
@@ -107,13 +109,54 @@ head(eff_resid) # check it
 
 # Phylogenetic ANOVA and post-hoc tests
 phylANOVA(pruned, dietV, eff_resid, nsim=1000, posthoc=TRUE, p.adj="holm")
+
 # The effect of diet on the residual distribution is not significant: F = 4.878, p = 0.349
-boxplot(eff_resid~dietV) 
+boxplot(LMresV~dietV, col=c(1,2,3), xaxt = 'n', pch = 20, font.lab = 2, whisklty = "solid", staplelwd = 2, whisklwd = 2, boxlwd = 2,
+        las = 1, ylab = NA, xlab = NA) 
+Axis(side=1, at = c(1,2,3), labels=c("Carnivore", "Herbivore", "Omnivore"))
+title(ylab=expression(bold(paste("Hindlimb residual distribution"))),
+      xlab=expression(bold(paste("Diet"))),
+      line=2.5)
 # plot it - easy to see that theres no significant effect of diet on the residual distribution, eg. carnivores
 # ('1') seem to be slightly more upright (compared to the linear model trend line), but not significantly
 # different from herbivores or omnivores.
+### 1####
+# and now checking residual distribution for habitat type
+# is there an effect of phylogeny on habitat preference? 
+habitat<-as.matrix(trait_order[,7])
+rownames(habitat)<-rownames(trait_order) 
+habitatV <- habitat[,1]
+colnames(habitat)<-"habitat" 
+head(habitat) 
+head(habitatV)
+phylosig(pruned, habitatV, method="lambda", test=TRUE, nsim=10000)
 
-# could also use phylo.resid
+phylANOVA(pruned, habitatV, eff_resid, nsim=1000, posthoc=TRUE, p.adj="holm")
+# The effect of diet on the residual distribution is not significant: F = 4.878, p = 0.349
+boxplot(eff_resid~habitat, col=c(1,2,3,4,5), xaxt = 'n', pch = 20, font.lab = 2, whisklty = "solid", staplelwd = 2, whisklwd = 2, boxlwd = 2,
+        las = 1, ylab = NA, xlab = NA) 
+
+Axis(side=1, at = c(1,2,3,4,5), labels=c("Forest", "Canopy", "Desert", "Plains", "Alpine"))
+title(ylab=expression(bold(paste("Hindlimb residual distribution"))),
+      xlab=expression(bold(paste("Habitat"))),
+      line=2.5)
+
+# is there an effect of phylogeny on fac? 
+fac<-as.matrix(trait_order[,8])
+rownames(fac)<-rownames(trait_order) 
+facV <- fac[,1]
+colnames(fac)<-"fac" 
+head(fac)
+head(facV)
+phylosig(pruned, facV, method="lambda", test=TRUE, nsim=10000)
+
+phylANOVA(pruned, facV, eff_resid, nsim=1000, posthoc=TRUE, p.adj="holm")
+boxplot(eff_resid~fac, col=c(1,2,3), xaxt = 'n', pch = 20, font.lab = 2, whisklty = "solid", staplelwd = 2, whisklwd = 2, boxlwd = 2,
+        las = 1, ylab = NA, xlab = NA) 
+Axis(side=1, at = c(1,2,3), labels=c("Fossorial", "Arboreal", "Cursorial"))
+title(ylab=expression(bold(paste("Hindlimb residual distribution"))),
+      xlab= expression(bold(paste("Predominant locomotion"))),
+      line=2.5)
 
 #### forelimb ####
 
@@ -147,13 +190,8 @@ cols=c("red","blue","green")
 # Scatterplot- same as the one we looked at previously, but coloured by diet
 summary(fit<-lm(eff~log10(mass),mydata)) # create a linear model, save the intercept and slope to 'fit'
 plot(eff~log10(mass),mydata, pch=19, col=mydata$diet, font.lab = 2, las = 1, ylab = expression(bold(paste("Effective hind-limb length"))), xlab = expression(bold(paste("Log10(body mass)"))))
-mydata$diet = factor(mydata$diet, levels = c('1','2', '3'),labels = c("Carnivore", "Herbivore", "Omnivore"))
-legend("topleft", legend=levels(mydata$diet), pch = 19, bty = "n", col = c(1:3))
-abline(fit, col="red", lwd=2)
-
-# or colour by order
-plot(eff~log10(mass),mydata, pch=19, col=mydata$order, font.lab = 2, las = 1, ylab = expression(bold(paste("Effective hind-limb length"))), xlab = expression(bold(paste("Log10(body mass)"))))
-legend(2.9,0.75,legend=levels(mydata$order), cex = 0.65, pch = 19, col = c(1:14), bty = "n")
+mydata$diet1 = factor(mydata$diet, levels = c('1','2', '3'),labels = c("Carnivore", "Herbivore", "Omnivore"))
+legend("topleft", legend=levels(mydata$diet1), pch = 19, bty = "n", col = c(1:3))
 abline(fit, col="red", lwd=2) # add a red abline to the data with 'fit's information
 # There's an upward trend- a slope of 0.08, and the summary(lm) indicates that this is significant (p < 2.2e-16)
 
@@ -196,6 +234,9 @@ head(dietV) # against the massV dataframe
 phylosig(pruned, dietV, method="lambda", test=TRUE, nsim=10000)
 # very unlikely that effective limb length is in this order by chance- the p-value is 6.614822e-43
 
+x<-anc.Bayes(pruned, effV, ngen = 10000)
+estimates<-colMeans(x[21:nrow(x),])
+
 # Performs phylogenetic RMA regression.
 # Phyl.RMA accounts for the effect of phylogeny on effective hip height as a function of mass. 
 output<-phyl.RMA(massV, effV, pruned) 
@@ -217,9 +258,51 @@ head(eff_resid) # check it
 # Phylogenetic ANOVA and post-hoc tests
 phylANOVA(pruned, dietV, eff_resid, nsim=1000, posthoc=TRUE, p.adj="holm")
 # The effect of diet on the residual distribution is not significant: F = 4.878, p = 0.349
-boxplot(eff_resid~dietV) 
+boxplot(eff_resid~diet, col=c(1,2,3), xaxt = 'n', pch = 20, font.lab = 2, whisklty = "solid", staplelwd = 2, whisklwd = 2, boxlwd = 2,
+        las = 1, ylab = NA, xlab = NA) 
+Axis(side=1, at = c(1,2,3), labels=c("Carnivore", "Herbivore", "Omnivore"))
+title(ylab=expression(bold(paste("Forelimb residual distribution"))),
+      xlab=expression(bold(paste("Diet"))),
+      line=2.5)
 # plot it - easy to see that theres no significant effect of diet on the residual distribution, eg. carnivores
 # ('1') seem to be slightly more upright (compared to the linear model trend line), but not significantly
 # different from herbivores or omnivores.
 
-# could also use phylo.resid
+# is there an effect of phylogeny on habitat preference? 
+habitat<-as.matrix(trait_order[,7])
+rownames(habitat)<-rownames(trait_order) 
+habitatV <- habitat[,1]
+colnames(habitat)<-"diet" 
+head(habitat) 
+head(habitatV)
+phylosig(pruned, habitatV, method="lambda", test=TRUE, nsim=10000)
+
+phylANOVA(pruned, habitatV, eff_resid, nsim=1000, posthoc=TRUE, p.adj="holm")
+
+boxplot(eff_resid~habitat, col=c(1,2,3,4,5), xaxt = 'n', pch = 20, font.lab = 2, whisklty = "solid", staplelwd = 2, whisklwd = 2, boxlwd = 2,
+        las = 1, ylab = NA, xlab = NA) 
+Axis(side=1, at = c(1,2,3,4,5), labels=c("Forest", "Canopy", "Desert", "Plains", "Alpine"))
+title(ylab=expression(bold(paste("Forelimb residual distribution"))),
+      xlab=expression(bold(paste("Habitat"))),
+      line=2.5)
+par()
+
+
+#effect on fac?
+fac<-as.matrix(trait_order[,8])
+rownames(fac)<-rownames(trait_order) 
+facV <- fac[,1]
+colnames(fac)<-"fac" 
+head(fac)
+head(facV)
+phylosig(pruned, facV, method="lambda", test=TRUE, nsim=10000)
+
+phylANOVA(pruned, facV, eff_resid, nsim=1000, posthoc=TRUE, p.adj="holm")
+boxplot(eff_resid~fac, col=c(1,2,3), xaxt = 'n', pch = 20, font.lab = 2, whisklty = "solid", staplelwd = 2, whisklwd = 2, boxlwd = 2,
+        las = 1, ylab = NA, xlab = NA) 
+Axis(side=1, at = c(1,2,3), labels=c("Fossorial", "Arboreal", "Cursorial"))
+title(ylab=expression(bold(paste("Forelimb residual distribution"))),
+      xlab= expression(bold(paste("Predominant locomotion"))),
+      line=2.5)
+
+
